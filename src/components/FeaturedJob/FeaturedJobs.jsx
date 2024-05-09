4e03a1ac0455e304fa8b6ed3479385e7f8c2bcde
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-05-09 19:08:36 +0600

    jobs data is fetched

--- src/components/FeaturedJob/FeaturedJobs.jsx
+++ src/components/FeaturedJob/FeaturedJobs.jsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from "react";
+
 function FeaturedJobs() {
-  return <div>FeaturedJobs</div>;
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
+  return (
+    <div>
+      <h1>Featured Jobs {jobs.length}</h1>
+    </div>
+  );
 }
 
 export default FeaturedJobs;
